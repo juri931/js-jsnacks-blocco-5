ea2bc1bfa8f80902ecca228779f661f4efc554c3
juri931 <impa.mat@hotmail.it>
2024-03-07 16:19:52 +0100

    [add] cassetta di zucchine

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+// Dichiarazione cassetta di zucchine
+const zucchine = [
+  {
+    varieta: "Nero di Milano",
+    peso: 200,
+    lunghezza: 20,
+  },
+  {
+    varieta: "Romanesco",
+    peso: 250,
+    lunghezza: 25,
+  },
+  {
+    varieta: "Striato di Napoli",
+    peso: 200,
+    lunghezza: 20,
+  },
+  {
+    varieta: "Bianco Triestino",
+    peso: 150,
+    lunghezza: 20,
+  },
+  {
+    varieta: "Pâtisson",
+    peso: 150,
+    lunghezza: 15,
+  },
+  {
+    varieta: "Crookneck",
+    peso: 200,
+    lunghezza: 20,
+  },
+  {
+    varieta: "Giallo",
+    peso: 200,
+    lunghezza: 20,
+  },
+  {
+    varieta: "Lungo Fiorentino",
+    peso: 250,
+    lunghezza: 25,
+  },
+  {
+    varieta: "Trombetta di Albenga",
+    peso: 250,
+    lunghezza: 25,
+  },
+  {
+    varieta: "Ad Alberello",
+    peso: 200,
+    lunghezza: 20,
+  },
+];
+
+// JSnack 1: Peso di tutte le zucchine
